2d10f4ec3bad79ebe3b0a023d071b641a8c45a65
Anthony Salemo <ajssalemo@gmail.com>
2021-08-09 16:12:27 -0400

    able to deploy a basic war with the maven plugin to Azure

--- mysql/src/main/java/com/mysqldatabaseexample/springbootwarfile/Controllers/HomeController.java
+++ mysql/src/main/java/com/mysqldatabaseexample/springbootwarfile/Controllers/HomeController.java
@@ -1,5 +1,14 @@
-// package com.mysqldatabaseexample.springbootwarfile.Controllers;
+package com.mysqldatabaseexample.springbootwarfile.Controllers;
 
-// public class HomeController {
-    
-// }
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    String message = "SpringBoot Warfile - MySQL example.";
+
+    @GetMapping("/")
+    public String index() {
+        return message;
+    }
+}
