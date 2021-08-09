3bdad8d7e9f1888d4aa1ba3e6cf6238b648cb4d7
Anthony Salemo <ajssalemo@gmail.com>
2021-08-09 15:49:39 -0400

    creating a quickstart .war/mysql project - editing pom.xml

---
+++ mysql/src/main/java/com/mysqldatabaseexample/springbootwarfile/Controllers/HomeController.java
@@ -0,0 +1,5 @@
+// package com.mysqldatabaseexample.springbootwarfile.Controllers;
+
+// public class HomeController {
+    
+// }
---
+++ mysql/src/main/java/com/mysqldatabaseexample/springbootwarfile/SpringbootwarfileApplication.java
@@ -0,0 +1,13 @@
+package com.mysqldatabaseexample.springbootwarfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwarfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwarfileApplication.class, args);
+	}
+
+}
