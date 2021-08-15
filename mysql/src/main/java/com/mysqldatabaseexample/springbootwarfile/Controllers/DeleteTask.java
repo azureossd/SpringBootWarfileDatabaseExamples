703c67b072590ad04838dbf0317ec81eb1ede21f
Anthony Salemo <ajssalemo@gmail.com>
2021-08-15 19:39:45 -0400

    added a DELETE controller, added proper annotation

--- mysql/src/main/java/com/mysqldatabaseexample/springbootwarfile/Controllers/DeleteTask.java
+++ mysql/src/main/java/com/mysqldatabaseexample/springbootwarfile/Controllers/DeleteTask.java
@@ -6,17 +6,19 @@ import com.mysqldatabaseexample.springbootwarfile.Entities.TasksEntity;
 import com.mysqldatabaseexample.springbootwarfile.Repository.TasksRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class FindTaskById {
+public class DeleteTask {
     @Autowired
     private TasksRepository tasks;
 
-    @GetMapping("/find_by_id/{id}")
-    public TasksEntity findAllTasks(@PathVariable int id) {
-        return tasks.findById(id).orElseThrow(() -> new EntityNotFoundException("Task not found with ID: " + id));
+    @DeleteMapping("/delete_task/{id}")
+    public TasksEntity deleteTask(@PathVariable int id) {
+        var findTask = tasks.findById(id).orElseThrow(() -> new EntityNotFoundException("Task not found with ID: " + id));
+        tasks.deleteById(findTask.getId());
+        return findTask;
     }
 }
