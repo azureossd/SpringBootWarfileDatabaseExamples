eabae744f6cf331fec0ee0008149b58e77597098
Anthony Salemo <ajssalemo@gmail.com>
2021-08-22 16:27:58 -0400

    added a Controller to find tasks by id

---
+++ postgres/src/main/java/com/postgresdatabaseexample/springbootwarfile/Controllers/FindTaskByIdController.java
@@ -0,0 +1,22 @@
+package com.postgresdatabaseexample.springbootwarfile.Controllers;
+
+import javax.persistence.EntityNotFoundException;
+
+import com.postgresdatabaseexample.springbootwarfile.Entities.TasksEntity;
+import com.postgresdatabaseexample.springbootwarfile.Repository.TasksRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FindTaskByIdController {
+    @Autowired
+    private TasksRepository tasks;
+
+    @GetMapping("/api/postgres/find/{id}")
+    public TasksEntity findTaskById(@PathVariable int id) {
+        return tasks.findById(id).orElseThrow(() -> new EntityNotFoundException("Task not found with ID: " + id));
+    }
+}
