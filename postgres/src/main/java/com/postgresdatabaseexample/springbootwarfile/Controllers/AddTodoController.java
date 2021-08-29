ec24ae6480b51abb1a1d8123fa083d0a1b4fa744
Anthony Salemo <ajssalemo@gmail.com>
2021-08-29 12:50:28 -0400

    added a new POST mapping for new todos

---
+++ postgres/src/main/java/com/postgresdatabaseexample/springbootwarfile/Controllers/AddTodoController.java
@@ -0,0 +1,23 @@
+package com.postgresdatabaseexample.springbootwarfile.Controllers;
+
+import com.postgresdatabaseexample.springbootwarfile.Entities.TasksEntity;
+import com.postgresdatabaseexample.springbootwarfile.Repository.TasksRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AddTodoController {
+    @Autowired
+    private TasksRepository tasks;
+
+    @PostMapping("/api/postgres/add_todo")
+    public ResponseEntity<TasksEntity> addTodo(@RequestBody TasksEntity newTodo) {
+        TasksEntity newAddedTodo = tasks.save(newTodo);
+        return new ResponseEntity<TasksEntity>(newAddedTodo, HttpStatus.CREATED);
+    }
+}
