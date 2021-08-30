4c355480508b1bd9e10c33d6e3a6e32de1c6614c
Anthony Salemo <ajssalemo@gmail.com>
2021-08-30 10:54:36 -0400

    updated the example to use hibernate validation

--- postgres/src/main/java/com/postgresdatabaseexample/springbootwarfile/Entities/TasksEntity.java
+++ postgres/src/main/java/com/postgresdatabaseexample/springbootwarfile/Entities/TasksEntity.java
@@ -5,15 +5,21 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "tasks")
 public class TasksEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    Integer Id;
-    String name;
-    Boolean completed;
+    private Integer Id;
+    @NotEmpty(message = "Value must be not null and not empty")
+    @Size(min = 2, max = 455, message = "Todo task must be between 2 and 500 characters long")
+    private String name;
+    @NotNull(message = "Value must either be true or false")
+    private Boolean completed;
 
     public Integer getId() {
         return Id;
